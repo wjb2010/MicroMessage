fce1e4484993279618abed58e1a4277dc0ad2fc0
wjb2010 <639586819@qq.com>
2016-11-04 17:32:49 +0800

    commit second

--- app/src/main/java/com/example/dllo/micromessage/Main/Activity/MainActivity.java
+++ app/src/main/java/com/example/dllo/micromessage/Main/Activity/MainActivity.java
@@ -72,4 +72,21 @@ private ViewPager vp;
 
 
 
+        tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
 }}
